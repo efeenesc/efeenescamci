f15eae2c0a00fa7d2a1aecf4406e239349789756
ecamci <efeenescamci@icloud.com>
2025-03-20 05:55:34 +0300

    Change favicon again, edit workflow to delete contents of dev folder before copy, fix error page, add meta tag to turn off Dark Reader, tidy package.json

--- src/app/routes/error-page/error-page.component.ts
+++ src/app/routes/error-page/error-page.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
-  selector: 'app-error-page',
-  imports: [],
+  selector: 'error-page',
+  imports: [RouterLink],
   templateUrl: './error-page.component.html',
-  styleUrl: './error-page.component.css'
 })
 export class ErrorPageComponent {
 
