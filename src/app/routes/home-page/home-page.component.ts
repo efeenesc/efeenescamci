789940dcad91b81b129d40afc92138e3f1f10a3e
ecamci <efeenescamci@icloud.com>
2025-08-14 23:03:30 +0300

    feat: add live video component, add two cat videos
    Works on long hover (default) or long press of 800ms (default).

--- src/app/routes/home-page/home-page.component.ts
+++ src/app/routes/home-page/home-page.component.ts
@@ -2,10 +2,16 @@ import { Component } from '@angular/core';
 import { ThemesComponent } from './sections/themes/themes.component';
 import { ExperimentsComponent } from './sections/experiments/experiments.component';
 import { BlogSectionComponent } from './sections/blog/blog.component';
+import { LivePhotoComponent } from '../../components/live-photo/live-photo.component';
 
 @Component({
 	selector: 'home-page',
-	imports: [ThemesComponent, ExperimentsComponent, BlogSectionComponent],
+	imports: [
+		ThemesComponent,
+		ExperimentsComponent,
+		BlogSectionComponent,
+		LivePhotoComponent,
+	],
 	templateUrl: './home-page.component.html',
 })
 export class HomePageComponent {}
