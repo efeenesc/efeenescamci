04641012f5c367f1801c0e5a4112be11ee986eb6
ecamci <efeenescamci@icloud.com>
2025-08-25 00:05:19 +0300

    feat: actually use the header in 404

--- src/app/routes/error-page/error-page.component.ts
+++ src/app/routes/error-page/error-page.component.ts
@@ -1,10 +1,16 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { PortalContentDirective } from '@directives/portal.directive';
+import { FakeLoadingBarService } from '@services/fake-loading-bar.service';
 
 @Component({
 	selector: 'error-page',
-	imports: [RouterLink],
+	imports: [RouterLink, PortalContentDirective],
 	templateUrl: './error-page.component.html',
 	changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class ErrorPageComponent {}
+export class ErrorPageComponent {
+	constructor(private loader: FakeLoadingBarService) {
+		this.loader.complete('custom');
+	}
+}
